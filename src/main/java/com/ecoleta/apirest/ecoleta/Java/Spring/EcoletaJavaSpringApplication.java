7f32c84c70ce6418278d9803982a299e0a629b4d
cunhaDev <dev.mateuscunha@gmail.com>
2021-04-19 07:28:52 -0400

    first commit ecoleta in Java with Spring

---
+++ src/main/java/com/ecoleta/apirest/ecoleta/Java/Spring/EcoletaJavaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.ecoleta.apirest.ecoleta.Java.Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoletaJavaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoletaJavaSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecoleta/apirest/ecoleta/Java/Spring/EcoletaJavaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecoleta.apirest.ecoleta.Java.Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoletaJavaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
